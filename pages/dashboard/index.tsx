f483f27eb70afc53bc8b6919e7dbfa9ae8d811be
gift56 <efeasiughu@gmail.com>
2023-08-17 10:43:01 -0700

    working on project azip

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,7 +4,14 @@ const DashboardPage = () => {
   return (
     <DashboardLayout>
       <header className="w-full bg-white px-10 py-3 flex items-center justify-between gap-4">
-        <img src="/imgs/companyLogo.svg" alt="/" className="hidden tab:flex" />
+        <img
+          src="/imgs/companyLogo.svg"
+          alt="/"
+          className="hidden tab:flex w-[110px] h-[58px] object-contain"
+        />
+        <div className="flex items-center justify-end gap-10">
+          
+        </div>
       </header>
     </DashboardLayout>
   );
