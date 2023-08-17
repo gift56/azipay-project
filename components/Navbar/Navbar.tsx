0bd26490b8606f89d1eae1e6223de4a7be8b15c9
gift56 <efeasiughu@gmail.com>
2023-08-16 18:07:38 -0700

    working on project azip

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [mobileNav, setMobileNav] = useState(false);
+
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside: EventListener = (event) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setMobileNav(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return <header className="w-full">Navbar</header>;
 };
 
 export default Navbar;
