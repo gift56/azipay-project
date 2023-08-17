ea004ec7eeec9c8d241885feb7ecae55ad4135a6
gift56 <efeasiughu@gmail.com>
2023-08-17 07:34:51 -0700

    working on project azip

--- components/LandingpageContent/GetExclusive/GetExclusive.tsx
+++ components/LandingpageContent/GetExclusive/GetExclusive.tsx
@@ -1,5 +1,23 @@
 const GetExclusive = () => {
-  return <section>GetExclusive</section>;
+  return (
+    <section className="w-full">
+      <div className="container">
+        <div className="w-full flex flex-col tab:flex-row tab:justify-between gap-6 tab:items-center items-start justify-start py-16 tab:py-28">
+          <div className="w-full tab:flex-1 flex flex-col items-start justify-start gap-8">
+            <h2 className="text-3xl font-bold text-primary md:text-4xl tab:text-[56px] tab:leading-[64px] font-play">
+              Get an Exclusive <br className="hidden tab:block" /> Demo of
+              Eazipay
+            </h2>
+            <p className="text-base font-normal text-gray1 md:text-lg tab:text-lg">
+              Our greatest priority is to put you and your business first. Let’s
+              show you how we can help.
+            </p>
+          </div>
+          <div className="w-full tab:flex-1"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GetExclusive;
