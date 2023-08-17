c4e96662c24ab45110bc01e52fade26dccec5637
gift56 <efeasiughu@gmail.com>
2023-08-17 10:33:46 -0700

    working on project azip

--- components/DashboardContent/DashboardSidebar/MobileDashNav/MobileDashNav.tsx
+++ components/DashboardContent/DashboardSidebar/MobileDashNav/MobileDashNav.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import Link from "next/link";
+import { GoHomeFill } from "react-icons/go";
 
 const MobileDashNav = () => {
-  return <nav className="lg:hidden">MobileDashNav</nav>;
+  return (
+    <nav className="lg:hidden w-full h-[110px] flex items-center justify-between gap-6">
+      <Link
+        href="/dashboard"
+        className="w-full flex flex-col items-center justify-center gap-1 text-primary text-sm font-medium"
+      >
+        <span>
+          <GoHomeFill size={20} />
+        </span>
+        <span>Home</span>
+      </Link>
+    </nav>
+  );
 };
 
 export default MobileDashNav;
