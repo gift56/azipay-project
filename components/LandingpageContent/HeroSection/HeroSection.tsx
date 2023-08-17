5826d65e5cf3af0cb5e0515b028a7b3c6401a46d
gift56 <efeasiughu@gmail.com>
2023-08-16 19:42:48 -0700

    working on project azip

--- components/LandingpageContent/HeroSection/HeroSection.tsx
+++ components/LandingpageContent/HeroSection/HeroSection.tsx
@@ -4,15 +4,17 @@ const HeroSection = () => {
   return (
     <section className="w-full">
       <div className="container">
-        <div className="w-full flex items-center justify-center py-10 tab:h-[90vh] flex-col tab:flex-row">
+        <div className="w-full flex items-center justify-center py-10 tab:h-[90vh] flex-col tab:flex-row gap-8">
           <div className="w-full tab:flex-1 flex flex-col items-start justify-start gap-8">
-            <h2 className="text-2xl font-bold text-dark md:text-4xl tab:text-6xl font-play">
+            <h2 className="text-3xl font-bold text-dark md:text-4xl tab:text-6xl font-play">
               Run your <span className="text-primary">payroll</span>{" "}
               <span className="text-army">easily</span> in{" "}
               <span className="text-danger">seconds</span>
             </h2>
           </div>
-          <div className="w-full tab:flex-1"></div>
+          <div className="w-full tab:flex-1">
+            <img src="/imgs/heroImg.png" alt="hero-image" />
+          </div>
         </div>
       </div>
     </section>
