ee7caf04660f35f3c9a2e93b98e4cca87dcb7693
gift56 <efeasiughu@gmail.com>
2023-08-17 07:27:51 -0700

    working on project azip

--- components/LandingpageContent/FreeForever/FreeForever.tsx
+++ components/LandingpageContent/FreeForever/FreeForever.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components";
+import Link from "next/link";
+
 const FreeForever = () => {
   return (
     <section className="w-full relative bg-[#FFE9E7]">
@@ -11,6 +14,56 @@ const FreeForever = () => {
                 salary <br className="hidden tab:block" /> payment
               </span>
             </h2>
+            <div className="w-full flex flex-col items-start justify-start gap-6 mt-5">
+              <h4 className="text-lg font-medium text-black">
+                Subscribe to the Eazilife today
+              </h4>
+              <div className="flex flex-col md:flex-row items-center justify-start gap-5">
+                <Link href="/">
+                  <Button
+                    text={
+                      <div className="flex items-center justify-start gap-5">
+                        <span className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center">
+                          <img src="/icons/appleIcon.svg" alt="apple_logo" />
+                        </span>
+                        <div className="flex items-start flex-col justify-start">
+                          <span className="text-xs font-medium text-gray1">
+                            Download on the
+                          </span>
+                          <span className="text-lg text-gray1 font-bold">
+                            Appstore
+                          </span>
+                        </div>
+                      </div>
+                    }
+                    className="bg-white rounded-md"
+                  />
+                </Link>
+                <Link href="/">
+                  <Button
+                    text={
+                      <div className="flex items-center justify-start gap-5">
+                        <span className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center">
+                          <img
+                            src="/icons/playstoreIcon.svg"
+                            alt="playstore_logo"
+                          />
+                        </span>
+                        <div className="flex items-start flex-col justify-start">
+                          <span className="text-xs font-medium text-gray1">
+                            Download on the
+                          </span>
+                          <span className="text-lg text-gray1 font-bold">
+                            Google Play
+                          </span>
+                        </div>
+                      </div>
+                    }
+                    className="bg-white rounded-md"
+                  />
+                </Link>
+              </div>
+            </div>
           </div>
           <div className="w-full tab:flex-1">
             <img src="/imgs/freeForeverImg.png" alt="/" />
