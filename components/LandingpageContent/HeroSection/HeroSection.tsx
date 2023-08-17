9d046d4bcda60b6a44544ebbee66bd71326ec74b
gift56 <efeasiughu@gmail.com>
2023-08-16 19:41:38 -0700

    working on project azip

--- components/LandingpageContent/HeroSection/HeroSection.tsx
+++ components/LandingpageContent/HeroSection/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <section className="w-full">
       <div className="container">
-        <div className="w-full flex items-center justify-center h-[90vh] flex-col tab:flex-row">
+        <div className="w-full flex items-center justify-center py-10 tab:h-[90vh] flex-col tab:flex-row">
           <div className="w-full tab:flex-1 flex flex-col items-start justify-start gap-8">
             <h2 className="text-2xl font-bold text-dark md:text-4xl tab:text-6xl font-play">
               Run your <span className="text-primary">payroll</span>{" "}
