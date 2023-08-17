26857f52edf9759683468329e9744a4a5f0d5aea
gift56 <efeasiughu@gmail.com>
2023-08-17 05:29:52 -0700

    working on project azip

--- components/LandingpageContent/Easypay/Easypay.tsx
+++ components/LandingpageContent/Easypay/Easypay.tsx
@@ -21,8 +21,20 @@ const Easypay = () => {
                 className="w-[447px]"
               />
             </div>
-            <div className="w-full tab:flex-1">
-              
+            <div className="w-full tab:flex-1 flex flex-col relative">
+              <div className="flex flex-col items-start justify-start gap-4">
+                <span className="md:w-14 md:h-14 w-12 h-12 flex items-center justify-center bg-primary text-white rounded-2xl text-xl font-bold md:text-2xl shadow-cardShad">
+                  1
+                </span>
+                <div>
+                  <h2 className="text-base font-bold text-gray1 md:text-lg tab:text-xl">
+                    Create your free account
+                  </h2>
+                  <p className="text-sm font-normal md:text-base text-gray1">
+                    Click here to set up your Eazipay account.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
