146c3611493d6802dc41ee57888be327ae0ccdd4
gift56 <efeasiughu@gmail.com>
2023-08-16 18:48:03 -0700

    working on project azip

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -7,7 +7,11 @@ const Footer = () => {
         <div className="w-full py-16 grid grid-cols-2 tab:grid-cols-3 xl:grid-cols-5">
           <div className="w-full flex flex-col items-start gap-4">
             <Link href="/">
-              <img src="/imgs/darkLogo.svg" alt="Easypay-logo" />
+              <img
+                src="/imgs/darkLogo.svg"
+                alt="Easypay-logo"
+                className="w-[122px]"
+              />
             </Link>
           </div>
         </div>
