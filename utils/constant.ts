a17760611befdb38e69f39e54c1bf9c93caae332
gift56 <efeasiughu@gmail.com>
2023-08-16 18:15:32 -0700

    working on project azip

---
+++ utils/constant.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    href: "/",
+    text: "Individual",
+  },
+  {
+    href: "/",
+    text: "Business",
+  },
+  {
+    href: "/",
+    text: "Pricing",
+  },
+  {
+    href: "/",
+    text: "Set your payroll",
+  },
+];
