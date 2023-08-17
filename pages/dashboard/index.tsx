ee382781f17539618e2600f688f0b73ac5440bd0
gift56 <efeasiughu@gmail.com>
2023-08-17 10:41:59 -0700

    working on project azip

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,13 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
 
 const DashboardPage = () => {
-  return <DashboardLayout>DashboardPage</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <header className="w-full bg-white px-10 py-3 flex items-center justify-between gap-4">
+        <img src="/imgs/companyLogo.svg" alt="/" className="hidden tab:flex" />
+      </header>
+    </DashboardLayout>
+  );
 };
 
 export default DashboardPage;
