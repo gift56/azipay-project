8cbb41d9c25f545a86c68c705aee78a88ed1e155
gift56 <efeasiughu@gmail.com>
2023-08-16 18:47:38 -0700

    working on project azip

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="w-full">
       <div className="container">
-        <div className="w-full py-16 grid grid-cols-2 tab:grid-cols-3 xl:grid-cols-5"></div>
+        <div className="w-full py-16 grid grid-cols-2 tab:grid-cols-3 xl:grid-cols-5">
+          <div className="w-full flex flex-col items-start gap-4">
+            <Link href="/">
+              <img src="/imgs/darkLogo.svg" alt="Easypay-logo" />
+            </Link>
+          </div>
+        </div>
       </div>
     </footer>
   );
