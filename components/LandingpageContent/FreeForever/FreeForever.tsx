6ee3a24b9feb4611f5d722f0d8ff68aeeb9dff81
gift56 <efeasiughu@gmail.com>
2023-08-17 07:22:39 -0700

    working on project azip

--- components/LandingpageContent/FreeForever/FreeForever.tsx
+++ components/LandingpageContent/FreeForever/FreeForever.tsx
@@ -1,6 +1,15 @@
 const FreeForever = () => {
   return (
-    <section className="w-full relative bg-[#FFE9E7]">FreeForever</section>
+    <section className="w-full relative bg-[#FFE9E7]">
+      <div className="container">
+        <div className="w-full flex flex-col tab:flex-row tab:justify-between gap-6 items-start justify-start">
+          <div className="w-full tab:flex-1"></div>
+          <div className="w-full tab:flex-1">
+            <img src="/imgs/freeForeverImg.png" alt="/" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
