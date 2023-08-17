4a67aaaed34d52656a2ab9d384d3262fe9675c3b
gift56 <efeasiughu@gmail.com>
2023-08-16 17:28:17 -0700

    working on project azip

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Head from "next/head";
+import React, { ReactNode } from "react";
 
-const MainLayout = () => {
-  return (
-    <div>MainLayout</div>
-  )
+interface LayoutProps {
+  children: ReactNode;
 }
 
-export default MainLayout
+const MainLayout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>EAZIPAY - Run your payroll easily in seconds</title>
+        <meta
+          name="description"
+          content="We’ve built an all-inclusive simple solution for individual and businesses to manage staff, pay salaries, bills, and relevant taxes all at once."
+        />
+      </Head>
+      
+      <main className="w-full">{children}</main>
+    </>
+  );
+};
+
+export default MainLayout;
