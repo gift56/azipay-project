5100ae09166effba74bc87e3fc8115d84f274b65
gift56 <efeasiughu@gmail.com>
2023-08-16 19:28:35 -0700

    working on project azip

--- components/LandingpageContent/HeroSection/HeroSection.tsx
+++ components/LandingpageContent/HeroSection/HeroSection.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <section className="w-full">
+      <div className="container">
+        <div className="w-full flex items-center justify-center h-[90vh] flex-col tab:flex-row">
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
