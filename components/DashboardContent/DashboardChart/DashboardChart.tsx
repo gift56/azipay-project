19d6f0c2bc0eb8eab1c46f669b170d85fd88e6e6
gift56 <efeasiughu@gmail.com>
2023-08-17 13:51:58 -0700

    working on project azip

--- components/DashboardContent/DashboardChart/DashboardChart.tsx
+++ components/DashboardContent/DashboardChart/DashboardChart.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const DashboardChart = () => {
   return (
-    <div className="bg-white w-full border border-[#E7E8E7] rounded-2xl p-8">
-      DashboardChart
-    </div>
+    <div className="bg-white w-full border border-[#E7E8E7] rounded-2xl p-8"></div>
   );
 };
 
