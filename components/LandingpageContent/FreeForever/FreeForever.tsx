24627a06582f28724f1ed817312952dedb696218
gift56 <efeasiughu@gmail.com>
2023-08-17 07:40:03 -0700

    working on project azip

--- components/LandingpageContent/FreeForever/FreeForever.tsx
+++ components/LandingpageContent/FreeForever/FreeForever.tsx
@@ -5,7 +5,7 @@ const FreeForever = () => {
   return (
     <section className="w-full relative bg-[#FFE9E7]">
       <div className="container">
-        <img src="/icons/freeImg.svg" alt="/" className="absoulte top-0" />
+        <img src="/icons/freeImg.svg" alt="/" className="absoulte top-0 w-[126px] aspect-square" />
         <div className="w-full flex flex-col tab:flex-row tab:justify-between gap-6 tab:items-center items-start justify-start py-16 tab:py-28">
           <div className="w-full tab:flex-1 flex flex-col items-start justify-start gap-8">
             <h2 className="text-3xl font-bold text-dark md:text-4xl tab:text-[56px] tab:leading-[64px] font-play">
--- components/LandingpageContent/GetExclusive/GetExclusive.tsx
+++ components/LandingpageContent/GetExclusive/GetExclusive.tsx
@@ -1,9 +1,9 @@
 const GetExclusive = () => {
   return (
     <section className="w-full relative">
-      <img src="/imgs/demoImg.svg" alt="/" className="absoulte top-0 w-[187px] aspect-square" />
+      <img src="/imgs/demoImg.svg" alt="/" className="absoulte top-20 w-[126px] md:w-[187px] aspect-square" />
       <div className="container">
-        <div className="w-full flex flex-col tab:flex-row tab:justify-between gap-6 tab:items-center items-start justify-start py-16 tab:py-28">
+        <div className="w-full flex flex-col tab:flex-row tab:justify-between gap-6 tab:items-center items-start justify-start py-12 tab:py-28">
           <div className="w-full tab:flex-1 flex flex-col items-start justify-start gap-8">
             <h2 className="text-3xl font-bold text-primary md:text-4xl tab:text-[56px] tab:leading-[64px] font-play">
               Get an Exclusive <br className="hidden tab:block" /> Demo of
