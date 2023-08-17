2e5bfa960259f9e2567ea0a3aadeba9edea00e63
gift56 <efeasiughu@gmail.com>
2023-08-16 18:44:05 -0700

    working on project azip

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-full">
+      <div className="container">
+        <div className="w-full py-16 grid grid-cols-2 tab:grid-cols-3 xl:grid-cols-5"></div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
